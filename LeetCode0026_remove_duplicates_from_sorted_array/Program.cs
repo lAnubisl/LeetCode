02a9fe4db2e2ca74b1c5d3aece7d30c95aa22a28
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2021-05-30 23:57:40 +0300

    add LeetCode 0026

---
+++ LeetCode0026_remove_duplicates_from_sorted_array/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace LeetCode0026_remove_duplicates_from_sorted_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = new int[] { 0,0,1,1,1,2,2,3,3,4 };
+            int len = RemoveDuplicates(nums);
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int RemoveDuplicates(int[] nums) 
+        {
+            if (nums.Length < 2)
+            {
+                return nums.Length;
+            }
+
+            int rightEdge = 0;
+            for(int i=1; i<nums.Length; i++)
+            {
+                if (nums[i] == nums[rightEdge])
+                {
+                    continue;
+                }
+
+                rightEdge++;
+                nums[rightEdge] = nums[i];
+            }
+
+            return rightEdge + 1;
+        }
+    }
+}
