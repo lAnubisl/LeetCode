267f426dbf10bf29c2df86a9f7d740ce43bb4a91
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2021-05-31 00:08:45 +0300

    add LeetCode 0027

---
+++ LeetCode0027_remove_element/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode0027_remove_element
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = new int[]{0,1,2,2,3,0,4,2};
+            var res = RemoveElement(nums, 2);
+            Console.WriteLine("Hello World!");
+        }
+
+        public static int RemoveElement(int[] nums, int val) 
+        {
+            int rightEdge = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == val)
+                {
+                    continue;
+                }
+
+                nums[rightEdge] = nums[i];
+                rightEdge++;
+            }
+
+            return rightEdge;
+        }
+    }
+}
