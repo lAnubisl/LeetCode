8f0c549144f87f1f6e1ed62d40c9af79fedfb30f
Alexander Panfilenok <alexander.panfilenok@gmail.com>
2021-06-03 12:52:27 +0300

    add LeetCode 0028 and 0029

---
+++ LeetCode0028_implement_strStr/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace LeetCode0028_implement_strStr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(StrStr("a", "a"));
+            Console.WriteLine(StrStr("hello", "ll"));
+        }
+
+        public static int StrStr(string haystack, string needle)
+        {
+            if (needle.Length == 0)
+            {
+                return 0;
+            }
+
+            for(int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if(haystack.Substring(i, needle.Length).Equals(needle))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode0029_divide_two_integers/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace LeetCode0029_divide_two_integers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Divide(-2147483648, 2) + " " + (-2147483648 / 2));
+            Console.WriteLine(Divide(10, 3) + " " + (10 / 3));
+            Console.WriteLine(Divide(2147483647, 2) + " " + (2147483647 / 2));
+            Console.WriteLine(Divide(2147483647, 3) + " " + (2147483647 / 3));
+        }
+
+        public static int Divide(int dividend, int divisor)
+        {
+            if (dividend == int.MinValue && divisor == -1) return int.MaxValue;
+            bool negativeResult = dividend > 0 ^ divisor > 0;
+            int negativeDividend = negative(dividend), negativeDivisor = negative(divisor), result = 0;
+            while(negativeDividend <= negativeDivisor)
+            {
+                int divisorBinaryShifted = negativeDivisor, binaryShifts = 0, temp = divisorBinaryShifted;
+                while(negativeDividend <= (temp <<= 1) && temp <= 0) 
+                {
+                    divisorBinaryShifted = temp;
+                    binaryShifts++;
+                }
+                    
+                negativeDividend -= divisorBinaryShifted;
+                result += 1 << binaryShifts;
+            }
+
+            return negativeResult ? negative(result) : result;
+        }
+
+        private static int negative(int x)
+        {
+            if (x < 0) return x;
+            var y = -1 >> 31;
+            return (x + y) ^ y;
+        }
+    }
+}
